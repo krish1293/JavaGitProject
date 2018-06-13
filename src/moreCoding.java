5363d1799884e7fb6ad4f9b44edc3fa4c9064f35
Krishna <kpwilkescs15@gmail.com>
2018-06-12 22:54:28 -0400

    New project

---
+++ src/moreCoding.java
@@ -0,0 +1,46 @@
+import linkList.Linkedlist;
+
+public class moreCoding{
+    
+    public static void main(String[] args) {
+    	
+    	// 1. Revese Linked List
+    	Linkedlist list = new Linkedlist();
+    	list.push(10);
+    	list.push(15);
+    	list.push(5);
+    	list.push(4);
+    	list.push(1);
+    	list.printList();
+    	list.reverseLL();
+    	list.printList();
+    } // main
+
+    // 1. Reverse linked list
+    
+
+    // 2. Given an array and int m, find avarage of m groups in o(n)
+
+    // 3. Given an array and target, find all pairs whose sum is equal to target.
+
+    // 4. Brackets Matching
+
+    // 5. Pair of range, merge them
+
+    // 6. Add two linked lists
+
+    // 7. First unique character in a string
+
+    // 8. Write a functions that finds and returns the node with the second highest value in a Binary Search Tree. 
+    // Assume BST is valid, but not necessarily complete or balanced.
+
+    // 9. Largest sum of path in 2D array.
+
+    // 10. Reverse the order of words in a sentence.
+
+    // 11. Validate BST
+
+    // 12. 
+
+} // moreCoding
+
