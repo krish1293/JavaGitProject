ddbdfab7a257b3ba84b9e22535886ac4be776636
Krishna <kpwilkescs15@gmail.com>
2018-06-13 22:23:54 -0400

    avgofMgrps

--- src/moreCoding.java
+++ src/moreCoding.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 import linkList.Linkedlist;
 
 public class moreCoding{
@@ -14,13 +16,38 @@ public class moreCoding{
     	list.printList();
     	list.reverseLL();
     	list.printList();
+    	
+    	// 2. Given an array and int m, find average of m groups in o(n)
+    	int[] arr = {2, 5, 7, 1, 9, 3, 9};
+        int m = 4;
+        float avg = avgofMgrps(arr, m);
+        System.out.println("Average of " + m + " groups in " + Arrays.toString(arr) + " is " + avg);
     } // main
 
     // 1. Reverse linked list
     
 
-    // 2. Given an array and int m, find avarage of m groups in o(n)
-
+    // 2. Given an array and int m, find average of m groups in o(n)
+    public static float avgofMgrps(int[] arr, int m) {
+    	
+    	float avgSum = 0;
+    	float sum = 0;
+    	int i = 0;
+    	
+    	for(; i < m; i++) {
+    		sum = sum + arr[i];
+    	}
+    	avgSum = avgSum + sum/m;
+    	
+    	for(; i < arr.length; i++) {
+    		sum = sum - arr[i-m] + arr[i];
+    		avgSum = avgSum + (sum/m);
+    	}
+    	
+    	return (avgSum / (arr.length - m + 1));
+    	
+    } // avgOfMgrps
+    
     // 3. Given an array and target, find all pairs whose sum is equal to target.
 
     // 4. Brackets Matching
